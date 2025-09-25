4733a76f8c1fb177e37cd80e2c9751f1fa3df7fc
cailbourdin <nathan.cailbourdin@recia.fr>
2025-09-25 15:59:15 +0200

    feat: check CAS session validity at startup

--- netocentre_app_poc/lib/main.dart
+++ netocentre_app_poc/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:netocentre_app_poc/pages/homePage.dart';
 import 'package:netocentre_app_poc/pages/loadingPage.dart';
 import 'package:netocentre_app_poc/pages/unconnectedHomePage.dart';
 import 'package:netocentre_app_poc/repositories/tokenRepository.dart';
-import 'package:netocentre_app_poc/services/portalService.dart';
+import 'package:netocentre_app_poc/services/loginService.dart';
 import 'package:netocentre_app_poc/singletons/tokenManager.dart';
 
 Future main() async {
@@ -11,39 +11,12 @@ Future main() async {
 
   print("token manager before loaded : ${TokenManager().toString()}");
   await TokenRepository().getLastValidRefreshToken();
-  // if we get a valid TGT from Database TODO: Need to verify if he's not expired
-  if(TokenManager().TGT != ""){
-    if(TokenManager().JSESSIONID == ""){
-      await PortalService().isAuthorizedByUPortal(); // Generate a new JSESSIONID
-    }
-  }else{
-    print("no valid TGT found");
-  }
   print("token manager after loaded : ${TokenManager().toString()}");
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-
-    if(TokenManager().TGT != "") { // TGT default value
-      return const MaterialApp(
-        title: 'Netocentre App POC',
-        debugShowCheckedModeBanner: false,
-        home: LoadingPage(callbackWidget: HomePage()),
-      );
-    }
-    else{
-      return const MaterialApp(
-        title: 'Netocentre App POC',
-        debugShowCheckedModeBanner: false,
-        home: UnconnectedHomePage(),
-      );
-    }
-  }
+  bool connected = await LoginService().hasCASSession();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: connected
+        ? const LoadingPage(callbackWidget: HomePage())
+        : const UnconnectedHomePage(),
+  ));
 }
