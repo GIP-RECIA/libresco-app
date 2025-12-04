0a24d416ca80f579db6f5a2f522e99797adbf0ba
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-12-04 15:49:15 +0100

    refactor: get config attribute more easily

--- netocentre_app_poc/lib/singletons/appConfig.dart
+++ netocentre_app_poc/lib/singletons/appConfig.dart
@@ -9,10 +9,10 @@ class AppConfig {
   final log = Logger('AppConfig');
   static final AppConfig _instance = AppConfig._internal();
 
+  String? _userAgent;
   String? _casBaseURL;
   String? _serviceURL;
   String? _uPortalBaseURL;
-  String? _userAgent;
 
   factory AppConfig() {
     return _instance;
@@ -22,60 +22,54 @@ class AppConfig {
 
   // All the config is loaded from one place : the app does not need to be updated is there is a configuration update
   Future<void> loadConfig() async {
-    log.fine("Generating user agent");
+    log.fine('Generating user agent');
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     String appName = packageInfo.appName;
     String version = packageInfo.version;
     String plateform = Platform.operatingSystem;
-    _userAgent = "$appName/$version ($plateform)";
-    log.fine("User agent ${_userAgent!} will be used for requests");
-    log.fine("Requesting config");
+    _userAgent = '$appName/$version ($plateform)';
+    log.fine('User agent ${_userAgent!} will be used for requests');
+
+    log.fine('Requesting config');
     final response = await http.get(
-      Uri.parse('https://lycees.test.recia.dev/commun/app_mobile_conf.json'),
+      Uri.parse('https://lycees.test.recia.dev/commun/app-mobile/conf.json'),
     );
     if (response.statusCode == 200) {
-      log.fine("Got an 200 answer for config. Decoding json...");
+      log.fine('Got an 200 answer for config. Decoding json...');
       final data = jsonDecode(response.body);
       _casBaseURL = data['casBaseURL'];
       _serviceURL = data['serviceURL'];
       _uPortalBaseURL = data['uPortalBaseURL'];
-      log.info(
-          "Config was successfully loaded with values _casBaseURL=$_casBaseURL, _serviceURL=$_serviceURL and _uPortalBaseURL=$_uPortalBaseURL");
+
+      log.info('Config was successfully loaded : ${toString()}');
     } else {
       throw Exception(
           'Error while loading config, error code is (${response.statusCode})');
     }
   }
 
-  String get casBaseURL {
-    if (_casBaseURL == null) {
-      throw Exception(
-          "_casBaseURL is not defined. Is the configuration loaded ?");
+  dynamic getAttribute(String name, dynamic value) {
+    if (value == null) {
+      throw Exception('$name is not defined. Is the configuration loaded ?');
     }
-    return _casBaseURL!;
+    return value;
   }
 
-  String get serviceURL {
-    if (_serviceURL == null) {
-      throw Exception(
-          "_serviceURL is not defined. Is the configuration loaded ?");
-    }
-    return _serviceURL!;
-  }
+  String get userAgent => getAttribute('_userAgent', _userAgent);
 
-  String get uPortalBaseURL {
-    if (_uPortalBaseURL == null) {
-      throw Exception(
-          "_uPortalBaseURL is not defined. Is the configuration loaded ?");
-    }
-    return _uPortalBaseURL!;
-  }
+  String get casBaseURL => getAttribute('_casBaseURL', _casBaseURL);
 
-  String get userAgent {
-    if (_userAgent == null) {
-      throw Exception(
-          "_userAgent is not defined. Is the configuration loaded ?");
-    }
-    return _userAgent!;
+  String get serviceURL => getAttribute('_serviceURL', _serviceURL);
+
+  String get uPortalBaseURL => getAttribute('_uPortalBaseURL', _uPortalBaseURL);
+
+  @override
+  String toString() {
+    return 'AppConfig{ '
+        '_userAgent: $_userAgent, '
+        '_casBaseURL: $_casBaseURL, '
+        '_serviceURL: $_serviceURL, ''
+        '_uPortalBaseURL: $_uPortalBaseURL'
+        ' }';
   }
 }
