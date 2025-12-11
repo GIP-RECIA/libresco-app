06179f2094d7042761fafbcc29b0ec0d65dd10b8
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-12-11 18:02:31 +0100

    feat: use app container in service web view

--- netocentre_app_poc/lib/pages/service_web_view.dart
+++ netocentre_app_poc/lib/pages/service_web_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:logging/logging.dart';
+import 'package:netocentre_app_poc/pages/components/app_container.dart';
 import 'package:netocentre_app_poc/singletons/app_config.dart';
 import 'package:netocentre_app_poc/singletons/session.dart';
 import 'package:netocentre_app_poc/utils/custom_cookies_manager.dart';
@@ -29,6 +30,7 @@ class _ServiceWebview extends State<ServiceWebview> {
   final log = Logger('_ServiceWebview');
   final GlobalKey webViewKey = GlobalKey();
   late CookieManager manager;
+  late String uri;
 
   InAppWebViewController? webViewController;
   InAppWebViewSettings settings = InAppWebViewSettings(
@@ -50,6 +52,7 @@ class _ServiceWebview extends State<ServiceWebview> {
     super.initState();
     _initCookies();
     _definePullToRefreshController();
+    _defineUri();
   }
 
   void _initCookies() {
@@ -63,155 +66,137 @@ class _ServiceWebview extends State<ServiceWebview> {
     pullToRefreshController = kIsWeb
         ? null
         : PullToRefreshController(
-      settings: PullToRefreshSettings(
-        color: Colors.blue,
-      ),
-      onRefresh: () async {
-        if (defaultTargetPlatform == TargetPlatform.android) {
-          webViewController?.reload();
-        } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-          webViewController?.loadUrl(
-            urlRequest: URLRequest(
-              url: await webViewController?.getUrl(),
+            settings: PullToRefreshSettings(
+              color: Colors.blue,
             ),
+            onRefresh: () async {
+              if (defaultTargetPlatform == TargetPlatform.android) {
+                webViewController?.reload();
+              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+                webViewController?.loadUrl(
+                  urlRequest: URLRequest(
+                    url: await webViewController?.getUrl(),
+                  ),
+                );
+              }
+            },
           );
-        }
-      },
+  }
+
+  void _defineUri() {
+    uri = '${AppConfig().uPortalBaseURL}/portail/p/${widget.uri}';
+    if (!widget.inPortal) {
+      uri = '${AppConfig().uPortalBaseURL}'
+          '/portail/api/ExternalURLStats'
+          '?fname=${widget.fname}'
+          '&service=${widget.uri}';
+    }
+
+    log.finer(
+      'on init ${widget.uri} : $uri | PortalSessionCookie : ${Session().PortalSessionCookie}',
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    log.finer('on init ${widget.uri} : ${Session().PortalSessionCookie}');
-    log.finer('on init ${widget.uri} : ${AppConfig().uPortalBaseURL}/');
-    log.finer(
-      'on init ${widget.uri} : ${AppConfig().uPortalBaseURL}/portail/p/${widget.uri}',
-    );
+    return AppContainer(
+      appBarBack: true,
+      onBack: () {
+        manager.removeSessionCookies();
+        Navigator.pop(context);
+      },
+      appBarTitle: widget.text,
+      body: Stack(
+        children: [
+          if (progress < 1.0) LinearProgressIndicator(value: progress),
+          InAppWebView(
+            key: webViewKey,
+            initialUrlRequest: URLRequest(url: WebUri(uri)),
+            initialSettings: settings,
+            pullToRefreshController: pullToRefreshController,
+            onWebViewCreated: (controller) async {
+              webViewController = controller;
+            },
+            onLoadStart: (controller, url) async {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onPermissionRequest: (controller, request) async {
+              return PermissionResponse(
+                resources: request.resources,
+                action: PermissionResponseAction.GRANT,
+              );
+            },
+            shouldOverrideUrlLoading: (controller, navigationAction) async {
+              var uri = navigationAction.request.url!;
 
-    String uri = '${AppConfig().uPortalBaseURL}/portail/p/${widget.uri}';
-    if(!widget.inPortal){
-      uri = '${AppConfig().uPortalBaseURL}/portail/api/ExternalURLStats?fname=${widget.fname}&service=${widget.uri}';
-    }
-
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Stack(
-                children: [
-                  InAppWebView(
-                    key: webViewKey,
-                    initialUrlRequest: URLRequest(
-                      url: WebUri(
-                          uri
-                      ),
-                    ),
-                    initialSettings: settings,
-                    pullToRefreshController: pullToRefreshController,
-                    onWebViewCreated: (controller) async {
-                      webViewController = controller;
-                    },
-                    onLoadStart: (controller, url) {
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onPermissionRequest: (controller, request) async {
-                      return PermissionResponse(
-                        resources: request.resources,
-                        action: PermissionResponseAction.GRANT,
-                      );
-                    },
-                    shouldOverrideUrlLoading:
-                        (controller, navigationAction) async {
-                      var uri = navigationAction.request.url!;
-
-                      if (![
-                        'http',
-                        'https',
-                        'file',
-                        'chrome',
-                        'data',
-                        'javascript',
-                        'about'
-                      ].contains(uri.scheme)) {
-                        if (await canLaunchUrl(uri)) {
-                          // Launch the App
-                          await launchUrl(
-                            uri,
-                          );
-                          // and cancel the request
-                          return NavigationActionPolicy.CANCEL;
-                        }
-                      }
-                      return NavigationActionPolicy.ALLOW;
-                    },
-                    onLoadStop: (controller, url) async {
-                      pullToRefreshController?.endRefreshing();
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onReceivedError: (controller, request, error) {
-                      pullToRefreshController?.endRefreshing();
-                    },
-                    onProgressChanged: (controller, progress) async {
-                      if (progress == 100) {
-                        pullToRefreshController?.endRefreshing();
-                      }
-                      setState(() {
-                        this.progress = progress / 100;
-                        urlController.text = url;
-                      });
-                    },
-                    onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                      setState(() {
-                        this.url = url.toString();
-                        urlController.text = this.url;
-                      });
-                    },
-                    onConsoleMessage: (controller, consoleMessage) {
-                      if (kDebugMode) {
-                        log.finer(
-                            'Console message ${consoleMessage.toString()}');
-                      }
-                    },
-                    onReceivedServerTrustAuthRequest:
-                        (controller, challenge) async {
-                      return ServerTrustAuthResponse(
-                        action: ServerTrustAuthResponseAction.PROCEED,
-                      );
-                    },
-                  ),
-                  progress < 1.0
-                      ? LinearProgressIndicator(value: progress)
-                      : Container(),
-                ],
-              ),
-            ),
-            OverflowBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  key: const Key('backFromWebviewButton'),
-                  child: Text('Quitter ${widget.text}'),
-                  onPressed: () {
-                    manager.removeSessionCookies();
-                    Navigator.of(context).pop();
-                  },
-                ),
-                ElevatedButton(
-                  child: const Icon(Icons.refresh),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
+              if (![
+                'http',
+                'https',
+                'file',
+                'chrome',
+                'data',
+                'javascript',
+                'about'
+              ].contains(uri.scheme)) {
+                if (await canLaunchUrl(uri)) {
+                  // Launch the App
+                  await launchUrl(
+                    uri,
+                  );
+                  // and cancel the request
+                  return NavigationActionPolicy.CANCEL;
+                }
+              }
+              return NavigationActionPolicy.ALLOW;
+            },
+            onLoadStop: (controller, url) async {
+              await controller.evaluateJavascript(
+                source:
+                    'document.querySelectorAll(\'r-header, r-footer, extended-uportal-header, extended-uportal-footer\').forEach(node => node.remove());',
+              );
+              pullToRefreshController?.endRefreshing();
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onReceivedError: (controller, request, error) {
+              pullToRefreshController?.endRefreshing();
+            },
+            onProgressChanged: (controller, progress) async {
+              if (progress == 100) {
+                pullToRefreshController?.endRefreshing();
+              }
+              setState(() {
+                this.progress = progress / 100;
+                urlController.text = url;
+              });
+            },
+            onUpdateVisitedHistory: (controller, url, androidIsReload) {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onConsoleMessage: (controller, consoleMessage) {
+              if (kDebugMode) {
+                log.finer(
+                  'Console message ${consoleMessage.toString()}',
+                );
+              }
+            },
+            onReceivedServerTrustAuthRequest: (controller, challenge) async {
+              return ServerTrustAuthResponse(
+                action: ServerTrustAuthResponseAction.PROCEED,
+              );
+            },
+          ),
+        ],
       ),
+      bottomNavigation: false,
     );
   }
 }
